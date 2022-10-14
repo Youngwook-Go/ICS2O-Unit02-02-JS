a91eceaf5af46f6d99a79536f3e6d98cfbae4ff5
Youngwook-Go <113055584+Youngwook-Go@users.noreply.github.com>
2022-10-14 03:44:01 +0000

    a

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2022 Youngwook All rights reserved
+//
+// Created by: Youngwook
+// Created on: Oct 2022
+// This file contains the JS functions for index1.html
+
+/**
+ * Area and Permeter
+ */
+function math() {
+  document.getElementById("area").innerHTML = "<p> The area is: " + (5 * 3) + "</p>"
+  document.getElementById("perimeter").innerHTML = "<p> The perimeter is: " + (2 *(5 + 3)) + "</p>"
+}
