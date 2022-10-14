801547f877d2f7bd0bdce685d4f557d86bcd3019
Youngwook-Go <Youngwook-Go@users.noreply.github.com>
2022-10-14 03:46:26 +0000

    Prettified Code!

--- JS/script.js
+++ JS/script.js
@@ -8,6 +8,8 @@
  * Area and Permeter
  */
 function math() {
-  document.getElementById("area").innerHTML = "<p> The area is: " + (5 * 3) + "</p>"
-  document.getElementById("perimeter").innerHTML = "<p> The perimeter is: " + (2 *(5 + 3)) + "</p>"
+  document.getElementById("area").innerHTML =
+    "<p> The area is: " + 5 * 3 + "</p>"
+  document.getElementById("perimeter").innerHTML =
+    "<p> The perimeter is: " + 2 * (5 + 3) + "</p>"
 }
